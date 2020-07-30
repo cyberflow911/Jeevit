293f48a40ce328a0ef48b1c6e5d999e10d41cc7a
unknown <cyber.flow911@gmail.com>
2020-07-30 21:55:22 +0530

    patient section base code

--- patient/index.php
+++ patient/index.php
@@ -1,13 +1,13 @@
 <?php
 require_once 'header.php'; 
 ?>
-<div class="content-wrapper">
+    <div class="content-wrapper">
 
 
+<!--        write your main content code inside this div-->
 
 
-
-</div>
+    </div>
 <?php
 	 
 	require_once 'js-links.php';
